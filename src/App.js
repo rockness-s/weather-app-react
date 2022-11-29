6ae916c723f8f20239c76543bc72cb4de91c4b7a
Inessa <breathe7underwater@gmail.com>
2022-11-29 20:38:35 +0100

    html

--- src/App.js
+++ src/App.js
@@ -12,6 +12,7 @@ export default function App() {
         <a href="https://github.com/rockness-s/weather-app-react" target="_blank" rel="noreferrer">GitHub</a>
         </footer>
     </div>
+
   );
 }
 
--- src/Weather.js
+++ src/Weather.js
@@ -3,6 +3,181 @@ import './App.css';
 
 export default function Weather() {
     return(
-        <p>Here will be App</p>
+    <div>
+        <div className='main-section'>
+            <div className='row'>
+            <div className='col-8'>
+            <h1>Dnipro</h1>
+            <h4>Overcast clouds</h4>
+            <p>29 Nov 2022, Tuesday 19:45</p>
+            </div>
+            <div className='col-2'>
+                <img src='https://ssl.gstatic.com/onebox/weather/64/cloudy.png' alt='Overcast clouds'></img>
+            </div>
+            <div className='col-2'>
+                <ul>
+                    <li>1°C</li>
+                    <li>RealFeel: -5°C</li>
+                </ul>
+            </div>
+            </div>
+
+            <div className='row'>
+                <div className='col-3'>
+                    <div>Wind</div>
+                    <div>24 km/h</div>
+                </div>
+                <div className='col-3'>
+                    <div>Humidity</div>
+                    <div>42%</div>
+                </div>
+                <div className='col-3'>
+                    <div>Sunrise</div>
+                    <div>06:07</div>
+                </div>
+                <div className='col-3'>
+                    <div>Sunset</div>
+                    <div>14:49</div>
+                </div>
+            </div>
+
+        </div>
+         <div className='forecast-section'>
+             <div className='row'>
+                 <div className='col-2'>
+                    <div>Tuesday</div>
+                    <div>29/11</div>
+                </div>
+                <div className='col-3'>
+                    <div>img</div>
+                </div>
+                <div className='col-2'>
+                    <div>Temp</div>
+                    <div>2/0°C</div>
+                </div>
+                <div className='col-3'>
+                    <div>Wind</div>
+                    <div>27 km/h</div>
+                </div>
+                <div className='col-2'>
+                    <div>Hum</div>
+                    <div>43%</div>
+                </div>
+                
+            </div>
+            <div className='row'>
+                 <div className='col-2'>
+                    <div>Tuesday</div>
+                    <div>29/11</div>
+                </div>
+                <div className='col-3'>
+                    <div>img</div>
+                </div>
+                <div className='col-2'>
+                    <div>Temp</div>
+                    <div>2/0°C</div>
+                </div>
+                <div className='col-3'>
+                    <div>Wind</div>
+                    <div>27 km/h</div>
+                </div>
+                <div className='col-2'>
+                    <div>Hum</div>
+                    <div>43%</div>
+                </div>
+                
+            </div>
+            <div className='row'>
+                 <div className='col-2'>
+                    <div>Tuesday</div>
+                    <div>29/11</div>
+                </div>
+                <div className='col-3'>
+                    <div>img</div>
+                </div>
+                <div className='col-2'>
+                    <div>Temp</div>
+                    <div>2/0°C</div>
+                </div>
+                <div className='col-3'>
+                    <div>Wind</div>
+                    <div>27 km/h</div>
+                </div>
+                <div className='col-2'>
+                    <div>Hum</div>
+                    <div>43%</div>
+                </div>
+                
+            </div>
+            <div className='row'>
+                 <div className='col-2'>
+                    <div>Tuesday</div>
+                    <div>29/11</div>
+                </div>
+                <div className='col-3'>
+                    <div>img</div>
+                </div>
+                <div className='col-2'>
+                    <div>Temp</div>
+                    <div>2/0°C</div>
+                </div>
+                <div className='col-3'>
+                    <div>Wind</div>
+                    <div>27 km/h</div>
+                </div>
+                <div className='col-2'>
+                    <div>Hum</div>
+                    <div>43%</div>
+                </div>
+                
+            </div>
+            <div className='row'>
+                 <div className='col-2'>
+                    <div>Tuesday</div>
+                    <div>29/11</div>
+                </div>
+                <div className='col-3'>
+                    <div>img</div>
+                </div>
+                <div className='col-2'>
+                    <div>Temp</div>
+                    <div>2/0°C</div>
+                </div>
+                <div className='col-3'>
+                    <div>Wind</div>
+                    <div>27 km/h</div>
+                </div>
+                <div className='col-2'>
+                    <div>Hum</div>
+                    <div>43%</div>
+                </div>
+                
+            </div>
+            <div className='row'>
+                 <div className='col-2'>
+                    <div>Tuesday</div>
+                    <div>29/11</div>
+                </div>
+                <div className='col-3'>
+                    <div>img</div>
+                </div>
+                <div className='col-2'>
+                    <div>Temp</div>
+                    <div>2/0°C</div>
+                </div>
+                <div className='col-3'>
+                    <div>Wind</div>
+                    <div>27 km/h</div>
+                </div>
+                <div className='col-2'>
+                    <div>Hum</div>
+                    <div>43%</div>
+                </div>
+                
+            </div>
+         </div>
+
+    </div>
+
     )
 }

91fe09b9e40fce33e4854ec25fb1a5937c09d74a
Inessa <breathe7underwater@gmail.com>
2022-11-29 19:28:51 +0100

    Added footer

--- src/App.js
+++ src/App.js
@@ -4,6 +4,12 @@ export default function App() {
   return (
     <div className="App">
       <h1>Weather</h1>
+
+      
+      <footer>Coded by Inessa Kazmiruk. 
+        <br></br>Open-sourced on {" "}
+        <a href="https://github.com/rockness-s/weather-app-react" target="_blank" rel="noreferrer">GitHub</a>
+        </footer>
     </div>
   );
 }

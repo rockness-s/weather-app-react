bca6d2e65faacab7b04ff4e865c95dfb3fc72916
Inessa <breathe7underwater@gmail.com>
2022-12-04 15:09:08 +0100

    Corrected date
    padstart

--- src/FormattedDate.js
+++ src/FormattedDate.js
@@ -3,8 +3,8 @@ import React from "react";
 export default function FormattedDate(props) {
     let days = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
     let day = days[props.date.getDay()];
-    let hours = props.date.getHours();
-    let minutes = props.date.getMinutes();
+    let hours = String(props.date.getHours()).padStart(2, "0");
+    let minutes = String(props.date.getMinutes()).padStart(2, "0");
     let date = props.date.getDate();
     let months = ["Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Oct","Nov","Dec"];
     let month = months[props.date.getMonth()];

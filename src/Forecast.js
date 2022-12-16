61af201cc4544b3533ae4fbbd2f791622a4fcf35
Inessa <breathe7underwater@gmail.com>
2022-12-16 12:38:42 +0100

    Forecast continue
    Temp and Icon

--- src/Forecast.js
+++ src/Forecast.js
@@ -1,43 +1,62 @@
-import React from "react";
+import React, { useState } from "react";
 import axios from "axios";
+// import './Weather.css';
 
 export default function Forecast(props) {
+    let [loaded, setLoaded] = useState(false);
+    let [forecast, setForecast] = useState(null);
+
     function showForecast(response) {
-        console.log(response.data)
+        console.log(response.data.daily)
+        setLoaded(true);
+        setForecast(response.data.daily)
     }
 
-    let apiKey = "01ce64btfcc913aa7034e9737a34of12";
-    let units = "metric";
-    let lat = props.coordinates.latitude;
-    let lon = props.coordinates.longitude;
-    let apiUrl = `https://api.shecodes.io/weather/v1/forecast?lon=${lon}&lat=${lat}&key=${apiKey}&units=${units}`;
-    axios.get(apiUrl).then(showForecast);
+    
 
-    return (
-        <div className='forecast-section'>
-        <div className='row'>
-                <div className='col-2'>
-                    <div className="fw-semibold text-decoration-underline">Tuesday</div>
-                    <div className="forecast-date">29/11</div>
-                    <div>img</div>
-                    <div className="forecast-temp"><span className="fw-semibold">2</span>/0°C</div>
-                </div>
-                <div className='col-2'>
+    if(loaded) {
+        return (
+            <div className='forecast-section'>
+            <div className='row'>
+                    <div className='col-2'>
+                        <div className="fw-semibold text-decoration-underline">Tuesday</div>
+                        <div className="forecast-date">29/11</div>
+                        <div className="forecast-icon">
+                            <img src={forecast[0].condition.icon_url} 
+                            alt='forecast icon'></img></div>
+                        <div className="forecast-temp">
+                            <span className="fw-semibold">{Math.round(forecast[0].temperature.maximum)}</span>
+                            /{Math.round(forecast[0].temperature.minimum)}°C</div>
+                    </div>
+                    <div className='col-2'>
+    
+                    </div>
+                    <div className='col-2'>
+    
+                    </div>
+                    <div className='col-2'>
+    
+                    </div>
+                    <div className='col-2'>
+    
+                    </div>
+                    <div className='col-2'>
+    
+                    </div>
+                    </div>
+                    </div>
+        )
 
-                </div>
-                <div className='col-2'>
+    } else {
+        let apiKey = "01ce64btfcc913aa7034e9737a34of12";
+        let units = "metric";
+        let lat = props.coordinates.latitude;
+        let lon = props.coordinates.longitude;
+        let apiUrl = `https://api.shecodes.io/weather/v1/forecast?lon=${lon}&lat=${lat}&key=${apiKey}&units=${units}`;
+        
+        axios.get(apiUrl).then(showForecast);
 
-                </div>
-                <div className='col-2'>
-
-                </div>
-                <div className='col-2'>
-
-                </div>
-                <div className='col-2'>
-
-                </div>
-                </div>
-                </div>
-    )
+        return null;
+    }
+    
 }
